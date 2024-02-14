107a3001a71fd864adb80606a7983d980646d331
Andimass <masyuckevich12345@gmail.com>
2024-02-14 15:48:05 +0300

    3_commit

--- src/components/screens/home/Header/Header.jsx
+++ src/components/screens/home/Header/Header.jsx
@@ -5,6 +5,27 @@ import $ from "jquery";
 import p5 from "p5";
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleModal = () => setIsOpen(!isOpen);
+
+    const [isOpen_2, setIsOpen_2] = useState(false);
+    const toggleModal_2 = () => setIsOpen_2(!isOpen_2);
+
+    const [isExpanded, setIsExpanded] = useState(false);
+    const toggleExpansion = () => setIsExpanded(!isExpanded);
+
+    const [isExpanded_2, setIsExpanded_2] = useState(false);
+    const toggleExpansion_2 = () => setIsExpanded_2(!isExpanded_2);
+
+    const [isOpen_3, setIsOpen_3] = useState(false);
+    const toggleModal_3 = () => setIsOpen_3(!isOpen_3);
+    const [isExpanded_3, setIsExpanded_3] = useState(false);
+    const toggleExpansion_3 = () => setIsExpanded_3(!isExpanded_3);
+
+    const [isOpen_4, setIsOpen_4] = useState(false);
+    const toggleModal_4 = () => setIsOpen_4(!isOpen_4);
+    const [isExpanded_4, setIsExpanded_4] = useState(false);
+    const toggleExpansion_4 = () => setIsExpanded_4(!isExpanded_4);
 
         return (
         <html>
@@ -21,6 +42,7 @@ const Header = () => {
             <div className='Header'>
                 <div className='header_up'>
                     <img className='header_up_img' src='/public/logo.png'/>
+                    <p className='header_up_img_title'>HR - агенство</p>
                     <div className='header_up_text'>
                         <a className='header_up_text_1' href='#about'>Почему мы</a>
                         <a className='header_up_text_2' href='#ourcep'>Этапы сотрудничества</a>
@@ -65,27 +87,11 @@ const Header = () => {
                 </div>
                 <div className='header_about_company'>
                     <img className='header_about_company_img' src='/public/6911306.jpg'/>
-                    <img className='header_about_company_img_2' src='/public/logo.png'/>
                     <h className='header_about_company_title' id='about'>О нас</h>
-                    <p className='header_about_company_text_1'>Мы - компания, специализирующаяся на подборе персонала любой сложности и широкого спектра</p>
-                    <p className='header_about_company_text_2'>Мы работаем со всеми отраслями и закрываем топ позиции и руководителей высшего звена</p>
-                    <p className='header_about_company_text_3'>Ценим экспертность и нацелены на долгосрочное сотрудничество с нашими клиентами.</p>
-                    <p className='header_about_company_text_4'><span>aaaaaaaaaa</span> — это профессиональное кадровое агентство, специализирующееся на подборе персонала любой сложности и широкого спектра. Наша экспертиза охватывает все отрасли, включая закрытие топ-менеджмента. Мы стремимся к долгосрочному сотрудничеству с нашими клиентами и ценим экспертность.</p>
-                </div>
-                <div className='header_ourcep'>
-                    <h2 className='header_ourcep_title' id='ourcep'>Наша цепочка сотрудничества</h2>
-                     <div className='header_ourcep_box'>
-                        <img className='header_ourcep_img_1' src='/public/dog.png'/>
-                        <p className='header_ourcep_text_1'>Подписание договора, заявки на подбор</p>
-                         <img className='header_ourcep_img_2' src='/public/brif.png'/>
-                        <p className='header_ourcep_text_2'>Брифинг по этапам отбора, выявления необходимых компетенций, оценке и сроков подбора</p>
-                         <img className='header_ourcep_img_3' src='/public/brif.png'/>
-                        <p className='header_ourcep_text_3'>Создание рабочей группы по кандидатам</p>
-                        <img className='header_ourcep_img_4' src='/public/brif.png'/>
-                        <p className='header_ourcep_text_4'>Подбор и отбор кандидатов, оценка кандидатов, приглашение на интервью</p>
-                         <img className='header_ourcep_img_5' src='/public/brif.png'/>
-                        <p className='header_ourcep_text_5'>Выход сотрудника на работу, сбор обратной связи и адаптационные встречи с кандидатом</p>
-                     </div>
+                    <p className='header_about_company_text_1'>Мы – рекрутинговая компания, осуществляем подбор русскоязычного персонала в различных сферах бизнеса. Найдём от рядового сотрудника до топ-менеджера и управленца. Сотрудничаем с представителями всех отраслей и направлений. </p>
+                    <p className='header_about_company_text_2'><span>INSTAFF</span> – это профессиональное HR-агентство. Наша цель – снять с собственника бизнеса «головную боль» по поиску и подбора персонала.</p>
+                    <p className='header_about_company_text_3'>Мы убеждены, что основная задача нашего заказчика – это развитие и масштабирование своей компании. Доверьте профессиональным рекрутёрам «рутину и операционку».</p>
+                    <p className='header_about_company_text_4'>Обратившись в кадровое агентство INSTAFF, вы сэкономите самый главный ресурс – время и энергию. А также существенно сократите бюджет на подбор и затраты на содержание штатного HR-специалиста.</p>
                 </div>
                 <div className='header_etap'>
                     <h2 className='header_etap_title' id='etap'>Этапы выполнения вакансии</h2>
@@ -157,8 +163,106 @@ const Header = () => {
                             <img className='header_preim_img_1' src='/public/business-analyst_11780004.png'/>
                         </div>
                 </div>
+                <div className='header_price'>
+                    <h2 className='header_price_title'>Расчет стоимости</h2>
+                      <div className='header_price_box'>
+                        <p className='header_price_text'>Стоимость услуги обсуждается индивидуально после анализа заявки на подбор.</p>
+                        <p className='header_price_factor'>Основные факторы, влияющие на ценообразование:</p>
+                        <p className='header_price_factor_n1'>1.	Регион поиска специалистов</p>
+                        <p className='header_price_factor_n2'>2.	Уникальность и специфика вакансии</p>
+                        <p className='header_price_factor_n3'>3.	Длительность гарантийного периода</p>
+                        </div>
+                    <img className='header_price_img' src='/public/raschet.jpg'/>
+                    <p className='header_price_price'>Рассчитать стоимость подбора</p>
+                </div>
+                <div className='header_otziv'>
+                    <h2 className='header_otziv_title'>Наши кейсы</h2>
+                    <div className='header_otziv_glav_box'>
+                    <div className={`header_otziv_box_1 ${isExpanded ? 'expanded' : ''}`}>
+                        <img className='header_otziv_box_1_title_img' src='/public/partner-logo.jpeg'/>
+                        <p className='header_otziv_box_1_title'>ООО "ПАРТНЕР"</p>
+                        <p className='header_otziv_box_1_title_date'>1 сентября</p>
+                        <img className='header_otziv_box_1_star_img' src='/public/stars_11906616.png'/>
+                        <p className='header_otziv_box_1_title_text'>Сделка состоялась</p>
+                        <img
+                            className={`header_otziv_box_1_img ${isOpen ? 'fullscreen' : ''}`}
+                            src='/public/partner-otz.jpeg'
+                            onClick={toggleModal}
+                            alt="Отзыв"
+                        />
+                        {isOpen && <div className="overlay" onClick={toggleModal}></div>}
+                        {isExpanded && <p className="additional_text">Менее, чем за месяц закрыли несколько позиций в инженерно-проектной компании. Удалось поработать с минимальными вложениями. Кандидатура сметчицы нашлась при помощи собственной базы соискателей.</p>}
+                        <p className='header_otziv_box_1_podrobnee' onClick={toggleExpansion}>{isExpanded ? 'Закрыть' : 'Подробнее'}</p>
+                    </div>
+                    <div className={`header_otziv_box_2 ${isExpanded_2 ? 'expanded_2' : ''}`}>
+                        <img className='header_otziv_box_2_title_img' src='/public/krovat.jpeg'/>
+                        <p className='header_otziv_box_1_title'>Кроваткинбург</p>
+                        <p className='header_otziv_box_1_title_date'>1 сентября</p>
+                        <img className='header_otziv_box_1_star_img' src='/public/stars_11906616.png'/>
+                        <p className='header_otziv_box_1_title_text'>Сделка состоялась</p>
+                        <img
+                            className={`header_otziv_box_2_img ${isOpen_2 ? 'fullscreen_2' : ''}`}
+                            src='/public/krov-otz.jpeg'
+                            onClick={toggleModal_2}
+                            alt="Отзыв"
+                        />
+                        {isOpen && <div className="overlay" onClick={toggleModal_2}></div>}
+                        {isExpanded_2 && <p className="additional_text">Искали менеджеров по продажам в интернет-магазин мебели. География поиска была: все регионы РФ, в которых московское время. Финальный запрос от заказчика был на поиск HRD. Подобрали успешного специалиста за 2 дня.</p>}
+                        <p className='header_otziv_box_1_podrobnee' onClick={toggleExpansion_2}>{isExpanded_2 ? 'Закрыть' : 'Подробнее'}</p>
+                    </div>
+                    <div className={`header_otziv_box_3 ${isExpanded_3 ? 'expanded_3' : ''}`}>
+                        <img className='header_otziv_box_2_title_img' src='/public/urlogo.jpeg'/>
+                        <p className='header_otziv_box_1_title'>ФИН Эксперт</p>
+                        <p className='header_otziv_box_1_title_date'>1 сентября</p>
+                        <img className='header_otziv_box_1_star_img' src='/public/stars_11906616.png'/>
+                        <p className='header_otziv_box_1_title_text'>Сделка состоялась</p>
+                        <img
+                            className={`header_otziv_box_3_img ${isOpen_3 ? 'fullscreen_3' : ''}`}
+                            src='/public/urotz.jpeg'
+                            onClick={toggleModal_3}
+                            alt="Отзыв"
+                        />
+                        {isOpen_3 && <div className="overlay" onClick={toggleModal_3}></div>}
+                        {isExpanded_3 && <p className="additional_text">Подобрали менеджера по работе с клиентами в юридическую компанию узкой специализации (банкротство). Несмотря на другой часовой пояс (+7 часов к московскому времени), сработали успешно и оперативно.</p>}
+                        <p className='header_otziv_box_1_podrobnee' onClick={toggleExpansion_3}>{isExpanded_3 ? 'Закрыть' : 'Подробнее'}</p>
+                    </div>
+                    <div className={`header_otziv_box_4 ${isExpanded_4 ? 'expanded_4' : ''}`}>
+                        <img className='header_otziv_box_4_title_img' src='/public/elit.jpeg'/>
+                        <p className='header_otziv_box_4_title'>ФИН Эксперт</p>
+                        <p className='header_otziv_box_4_title_date'>1 сентября</p>
+                        <img className='header_otziv_box_1_star_img' src='/public/stars_11906616.png'/>
+                        <p className='header_otziv_box_1_title_text'>Сделка состоялась</p>
+                        <img
+                            className={`header_otziv_box_4_img ${isOpen_4 ? 'fullscreen_4' : ''}`}
+                            src='/public/elitotz.jpeg'
+                            onClick={toggleModal_4}
+                            alt="Отзыв"
+                        />
+                        {isOpen_4 && <div className="overlay" onClick={toggleModal_4}></div>}
+                        {isExpanded_4 && <p className="additional_text">Был запрос от заказчика – подбор двух менеджеров по продажам в шоурум элитных аксессуаров для дома и офиса из натуральной кожи. Выполнили задачу за неделю.</p>}
+                        <p className='header_otziv_box_1_podrobnee' onClick={toggleExpansion_4}>{isExpanded_4 ? 'Закрыть' : 'Подробнее'}</p>
+                    </div>
+                </div>
+                </div>
+                <div className='header_op'>
+                    <h2 className='header_op_title'>Нам доверяют</h2>
+                    <div className='header_op_img_box'>
+                        <img className='header_op_img_box_1' src='/public/krovati.jpeg'/>
+                        <img className='header_op_img_box_2' src='/public/zapad.jpeg'/>
+                        <img className='header_op_img_box_3' src='/public/partner.jpeg'/>
+                        <img className='header_op_img_box_4' src='/public/sfera.jpeg'/>
+                        <img className='header_op_img_box_5' src='/public/zabor.jpeg'/>
+                        <img className='header_op_img_box_6' src='/public/finexp.jpeg'/>
+                        <img className='header_op_img_box_7' src='/public/giulia.jpeg'/>
+                    </div>
+                </div>
+                <div>
+
+                </div>
                 <div className='header_footer'>
                     <p>Подвал</p>
+                </div>
+                <div>
 
                 </div>
             </div>
